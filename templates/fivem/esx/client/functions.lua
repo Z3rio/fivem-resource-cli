0e394a4aa76745ff54e03e0800947ce63913da57
SYNO <89760730+SYNO-SY@users.noreply.github.com>
2022-10-27 17:24:50 +0400

    Update functions.lua

--- templates/fivem/esx/client/functions.lua
+++ templates/fivem/esx/client/functions.lua
@@ -1,12 +1,5 @@
 -- Compatibility for old ESX versions
-if ESX == nil then
-    Citizen.CreateThread(function()    
-        while ESX == nil do 
-            TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
-            Citizen.Wait(500)
-        end
-    end)
-end
+ESX = exports["es_extended"]:getSharedObject()
 
 Functions = {
     
