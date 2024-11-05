3dce7311b5ab38a22d5309901eeecfc4fc320bbf
Z3rio <neoboij@gmail.com>
2024-11-05 14:27:52 +0100

    feat: finalize locale & version checker snippets

--- src/files/fivem/addons/versionChecking.ts
+++ src/files/fivem/addons/versionChecking.ts
@@ -1,7 +1,45 @@
 import { Addon, File } from "../../../utils/types.js";
 
-const files: File[] = []
+const files: File[] = [
+  {
+    name: "server/versionchecker.lua",
+    content: `local rawFileLink = "RAW LINK TO CURRENT VERSION HERE"
+local currVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
+if currVersion == nil then
+  warn("Failed to find script version")
+end
 
+PerformHttpRequest(
+  rawFileLink,
+  function(code, newestVersion, headers)
+    if code == 200 then
+      if tostring(currVersion) == tostring(newestVersion) then
+        print("The script is up to date")
+      else
+        print(
+          "The script has updated. You are on version "
+            .. tostring(currVersion)
+            .. ", the newest version is "
+            .. tostring(newestVersion)
+        )
+      end
+    else
+      print("Failed to fetch current version")
+    end
+  end,
+  "GET"
+)`
+  },
+  {
+    name: "fxmanifest.lua",
+    content: `server_scripts({
+    "server/versionchecker.lua"
+})`,
+    comment: "Addon: Version Checker"
+  },
+]
+
+// todo: ask user about version the values above ^^
 export default {
   label: "Version Checking",
   name: "versionchecking",
